05f105c23cf5a896f6a7d518a77c12dfb3240914
goldbach8 <marianodeiriondo8@gmail.com>
2023-05-10 23:40:51 -0300

    FrontEnd

---
+++ src/app/service/persona.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { persona } from '../model/persona.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class PersonaService {
+  URL = 'http://localhost:8080/personas/';
+
+  constructor(private http: HttpClient) { }
+
+  public getPersona(): Observable<persona>{
+    return this.http.get<persona>(this.URL+ 'traer/perfil');
+  }
+
+  public update(id: number, persona: persona): Observable<any>{
+    return this.http.put<any>(this.URL + `update/${id}`, persona);
+  }
+}
