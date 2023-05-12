e069ebf2a1480a1f159bdd14ac176be2cf4c7e3d
goldbach8 <marianodeiriondo8@gmail.com>
2023-05-11 23:13:53 -0300

    .

--- src/app/components/acerca-de/acerca-de.component.ts
+++ src/app/components/acerca-de/acerca-de.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { persona } from 'src/app/model/persona.model';
 import { PersonaService } from 'src/app/service/persona.service';
+import { TokenService } from 'src/app/service/token.service';
 
 @Component({
   selector: 'app-acerca-de',
@@ -10,10 +11,18 @@ import { PersonaService } from 'src/app/service/persona.service';
 export class AcercaDeComponent implements OnInit {
   persona: persona = null;
   
-  constructor(public personaService: PersonaService) { }
+  constructor(public personaService: PersonaService,
+    private tokenService: TokenService) { }
+
+  isLogged = false;  
 
   ngOnInit(): void {
     this.personaService.getPersona().subscribe(data => {this.persona = data})
+    if(this.tokenService.getToken()){
+      this.isLogged = true;
+    } else{
+      this.isLogged = false;
+    }
   }
 
 }
