b0db9640866b2c9a62e26e45956863a3f2447e85
goldbach8 <marianodeiriondo8@gmail.com>
2023-05-11 22:58:00 -0300

    .

--- src/app/service/proyecto.service.ts
+++ src/app/service/proyecto.service.ts
@@ -8,7 +8,7 @@ import { Proyecto } from '../model/proyecto';
   providedIn: 'root'
 })
 export class ProyectoService {
-  URL = 'http://localhost:8080/proy/';
+  URL = 'https://frontendmdi.web.app/proy/';
 
   constructor(private httpClient : HttpClient) { }
 
--- src/app/service/skill.service.ts
+++ src/app/service/skill.service.ts
@@ -8,7 +8,7 @@ import { Skill } from '../model/skill';
   providedIn: 'root'
 })
 export class SkillService {
-  URL ='http://localhost:8080/skill/';
+  URL ='https://frontendmdi.web.app/skill/';
   
   constructor(private httpClient: HttpClient) { }
 
